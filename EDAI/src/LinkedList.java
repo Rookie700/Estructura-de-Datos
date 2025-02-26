26c67fcf8a258b24b48ddc6f43869c7cd2edcca3
rookie700 <igor.kam800@gmail.com>
2025-02-26 09:12:59 -0500

    ayuda por favor

--- EDAI/src/LinkedList.java
+++ EDAI/src/LinkedList.java
@@ -7,6 +7,53 @@
  *
  * @author estudiante
  */
-public class LinkedList {
+public class LinkedList<E> {
+
+    private Node<E> head;
+    private Node<E> tail;
+    private int size;
+
+    /**
+     * @return the head
+     */
+    public Node<E> getHead() {
+        return head;
+    }
+
+    /**
+     * @param head the head to set
+     */
+    public void setHead(Node<E> head) {
+        this.head = head;
+    }
+
+    /**
+     * @return the tail
+     */
+    public Node<E> getTail() {
+        return tail;
+    }
+
+    /**
+     * @param tail the tail to set
+     */
+    public void setTail(Node<E> tail) {
+        this.tail = tail;
+    }
+
+    /**
+     * @return the size
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * @param size the size to set
+     */
+    public void setSize(int size) {
+        this.size = size;
+    }
     
+
 }
